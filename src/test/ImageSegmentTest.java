87437ee05de544db21da7b4376da290298134246
hmgonzalez1 <hmgonzalez1@my.waketech.edu>
2017-10-02 18:28:35 -0400

    Worked on ImageSegmentTest and TextSegmentTest classes

--- src/test/ImageSegmentTest.java
+++ src/test/ImageSegmentTest.java
@@ -8,5 +8,25 @@ import data.ImageSegment;
 
 public class ImageSegmentTest {
 
-//
+    private ImageSegment is;
+
+    @BeforeClass
+    public void setup()
+    {
+        is = new ImageSegment(60, "static", "fade", "C:/Capstone/Messages/WTCC Logo.jpg");
+    }
+
+    @Test
+    public void testImageSegmentGetters()
+    {
+        assertEquals("data.ImageSegment test: getSource", "C:/Capstone/Messages/WTCC Logo.jpg", is.getSource());
+    }
+
+    @Test
+    public void testImageSegmentSetters()
+    {
+        is.setSource("C:/Capstone/Desktop/WTCC Logo.jpg");
+
+        assertEquals("data.ImageSegment test: setSource", "C:/Capstone/Messages/Flower.jpg", is.getSource());
+    }
 }
--- src/test/TextSegmentTest.java
+++ src/test/TextSegmentTest.java
@@ -8,5 +8,26 @@ import data.TextSegment;
 
 public class TextSegmentTest {
 
+    private TextSegment ts;
+
+    @BeforeClass
+    public void setup()
+    {
+        ts = new TextSegment(60, "left-scroll-continuous", "fade", "Welcome to VISION");
+    }
+
+    @Test
+    public void testTextSegmentGetters()
+    {
+        assertEquals("data.TextSegment test: getText", "Welcome to VISION", ts.getText());
+    }
+
+    @Test
+    public void testTextSegmentSetters()
+    {
+        ts.setText("Hello World");
+
+        assertEquals("data.TextSegment test: setText", "Hello World", ts.getText());
+    }
 //
 }
