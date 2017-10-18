e48d3c8a02709ed52b35c7a6731bff95a4956b24
lewalters <lewalters@my.waketech.edu>
2017-10-18 15:01:13 -0400

    Added RGB class to help with image conversion

---
+++ src/util/RGB.java
@@ -0,0 +1,29 @@
+package util;
+
+public class RGB
+{
+    public double r, g, b, o;
+
+    public RGB()
+    {
+        r = 0;
+        g = 0;
+        b = 0;
+        o = 0;
+    }
+
+//    public void addRed(int red)
+//    {
+//        this.red += red;
+//    }
+//
+//    public void addGreen(int green)
+//    {
+//        this.green += green;
+//    }
+//
+//    public void addBlue(int blue)
+//    {
+//        this.blue += blue;
+//    }
+}
